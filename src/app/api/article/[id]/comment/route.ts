52db06122f430f02b2f048bd674e3747f54dc5b5
Starz099 <mayankjod016@gmail.com>
2025-09-23 01:17:50 +0530

    comments

--- src/app/api/article/[id]/comment/route.ts
+++ src/app/api/article/[id]/comment/route.ts
@@ -4,6 +4,62 @@ import { getServerSession } from "next-auth";
 
 const prisma = new PrismaClient();
 
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  const { id } = await params;
+
+  if (!id) {
+    return NextResponse.json({ error: "ID is required" }, { status: 400 });
+  }
+
+  try {
+    const session = await getServerSession();
+
+    if (!session) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = (
+      await prisma.user.findFirst({
+        where: { email: session?.user?.email },
+      })
+    )?.id;
+
+    if (!userId) {
+      return NextResponse.json({ error: "User not found" }, { status: 404 });
+    }
+
+    const comments = await prisma.comment.findMany({
+      where: {
+        articleId: id,
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true, // username field from User
+            email: true, // you can include more if needed
+          },
+        },
+      },
+    });
+
+    return NextResponse.json({
+      comments,
+      message: `Comments fetched for article id ${id}`,
+      status: 200,
+    });
+  } catch (e) {
+    console.error(`Error while fetching articles in api`, e);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+}
+
 export async function POST(
   request: NextRequest,
   { params }: { params: Promise<{ id: string }> },
@@ -38,7 +94,7 @@ export async function POST(
       data: {
         content,
         articleId,
-        userId,
+        authorId: userId,
       },
     });
 
--- src/components/ui/comment-box.tsx
+++ src/components/ui/comment-box.tsx
@@ -1,11 +1,21 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Input } from "./input";
 import { Button } from "./button";
 import axios from "axios";
+import { User } from "@prisma/client";
+
+interface Comment {
+  id: string;
+  content: string;
+  author: User;
+  authorId: string;
+  parentCommentId?: string;
+}
 
 const CommentBox = (props: { submissionId: string }) => {
   const [content, setContent] = useState("");
+  const [comments, setComments] = useState<Comment[]>([]);
 
   async function handleComment() {
     try {
@@ -17,6 +27,22 @@ const CommentBox = (props: { submissionId: string }) => {
     }
   }
 
+  useEffect(() => {
+    async function getComments() {
+      try {
+        const cmnts = (
+          await axios.get<{ comments: Comment[] }>(
+            `/api/article/${props.submissionId}/comment`,
+          )
+        ).data;
+        setComments(cmnts.comments.reverse());
+      } catch (e) {
+        console.log("error adding comment", e);
+      }
+    }
+
+    getComments();
+  }, [props.submissionId]);
   return (
     <div className="m-4 flex flex-col gap-4 bg-purple-50 p-4">
       <div className="flex gap-2">
@@ -26,9 +52,22 @@ const CommentBox = (props: { submissionId: string }) => {
         ></Input>
         <Button onClick={handleComment}>Comment</Button>
       </div>
-      <div>All Comments</div>
+      <div>
+        {comments.map((comment, key) => {
+          return (
+            <div
+              key={key}
+              className="mb-3 rounded-lg border border-purple-200 bg-white p-4 shadow-sm"
+            >
+              <p className="text-gray-800">{comment.content}</p>
+              <span className="mt-2 block text-sm text-gray-500">
+                — {comment.author.name}
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
-// TODO: comments should appear from database
 export default CommentBox;
